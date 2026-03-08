cb0f4b08ed180ce1d3b599bea549799dcb0a3e77
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-03-07 21:04:46 -0600

    (Sección 5: Expandir bases) @for - Realizar iteraciones de elementos

--- 01-bases/src/app/pages/dragonball-page/dragonball-page.ts
+++ 01-bases/src/app/pages/dragonball-page/dragonball-page.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+export interface ICharacter {
+  id: number;
+  name: string;
+  power: number;
+}
+
+const INITIAL_CHARACTERS: ICharacter[] = [
+  { id: 1, name: 'Goku', power: 9000 },
+  { id: 2, name: 'Vegeta', power: 8500 },
+  { id: 3, name: 'Gohan', power: 7000 },
+  { id: 4, name: 'Piccolo', power: 6000 },
+  { id: 5, name: 'Krillin', power: 5000 },
+];
 
 @Component({
   selector: 'app-dragonball-page',
   imports: [],
   templateUrl: './dragonball-page.html',
 })
-export class DragonballPage {}
+export class DragonballPage {
+  readonly characters = signal<ICharacter[]>(INITIAL_CHARACTERS);
+}
