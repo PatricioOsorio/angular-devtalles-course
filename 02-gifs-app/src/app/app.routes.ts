f546a3c396f9834a28d2486e58ecb62087fe7af1
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-03-08 20:59:45 -0600

    (Sección 6: GifsApp - Pensemos en componentes) Pensemos en componentes

--- 02-gifs-app/src/app/app.routes.ts
+++ 02-gifs-app/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('@app/gifs/pages/dashboard-page/dashboard-page'),
+  },
+  {
+    path: 'trending',
+    loadComponent: () => import('@app/gifs/pages/trending-page/trending-page'),
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('@app/gifs/pages/search-page/search-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
