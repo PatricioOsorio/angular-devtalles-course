302935d8da8ef4c66745964de1943350a32cb187
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-03-08 21:41:51 -0600

    (Sección 6: GifsApp - Pensemos en componentes) Rutas hijas

--- 02-gifs-app/src/app/app.routes.ts
+++ 02-gifs-app/src/app/app.routes.ts
@@ -4,15 +4,22 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     loadComponent: () => import('@app/gifs/pages/dashboard-page/dashboard-page'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('@app/gifs/pages/trending-page/trending-page'),
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('@app/gifs/pages/search-page/search-page'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      },
+    ],
   },
-  {
-    path: 'trending',
-    loadComponent: () => import('@app/gifs/pages/trending-page/trending-page'),
-  },
-  {
-    path: 'search',
-    loadComponent: () => import('@app/gifs/pages/search-page/search-page'),
-  },
+
   {
     path: '**',
     redirectTo: 'dashboard',
--- 02-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ 02-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LayoutPanelTop, LucideAngularModule, LucideIconNode } from 'lucide-angular';
+
+interface IMenuItem {
+  label: string;
+  description: string;
+  icon: readonly LucideIconNode[];
+  path: string;
+}
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [RouterOutlet, LucideAngularModule, RouterLink, RouterLinkActive],
   templateUrl: './dashboard-page.html',
 })
-export default class DashboardPage {}
+export default class DashboardPage {
+  readonly LayoutPanelTop = LayoutPanelTop;
+
+  readonly menuItems: IMenuItem[] = [
+    {
+      label: 'Trending',
+      description: 'See the trending gifs',
+      icon: LayoutPanelTop,
+      path: '/dashboard/trending',
+    },
+    {
+      label: 'Search',
+      description: 'Search for your favorite gifs',
+      icon: LayoutPanelTop,
+      path: '/dashboard/search',
+    },
+  ];
+}
