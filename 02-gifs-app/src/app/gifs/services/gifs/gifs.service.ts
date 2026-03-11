815b01f9068eadb62f0bdf8520418da7574d432d
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-11 09:25:44 -0600

    (Sección 6: GifsApp - Pensemos en componentes) Mostrar Gifs en pantalla

--- 02-gifs-app/src/app/gifs/services/gifs/gifs.service.ts
+++ 02-gifs-app/src/app/gifs/services/gifs/gifs.service.ts
@@ -3,7 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
 import { environment } from '@environments/environment';
-import type { IGiphyResponse, IGyphy } from '@app/gifs/interfaces/giphy.response';
+import type { IGiphyResponse } from '@app/gifs/interfaces/giphy.response';
 import { IGif } from '@app/gifs/interfaces/gif.interface';
 import { GifMapper } from '@app/gifs/mapper/gif.mapper';
 
@@ -12,27 +12,6 @@ import { GifMapper } from '@app/gifs/mapper/gif.mapper';
 })
 export class GifsService {
   readonly http = inject(HttpClient);
-  // readonly trendingGifs = signal<IGif[]>([]);
-
-  // constructor() {
-  //   this.loadTrendingGifs();
-  // }
-
-  // loadTrendingGifs() {
-  //   this.http
-  //     .get<IGiphyResponse>(`${environment.giphyApiUrl}/gifs/trending`, {
-  //       params: {
-  //         api_key: environment.giphyApiKey,
-  //         limit: 25,
-  //         rating: 'g',
-  //       },
-  //     })
-  //     .pipe(map((response) => GifMapper.toGifs(response.data)))
-  //     .subscribe({
-  //       next: (gifs) => this.trendingGifs.set(gifs),
-  //       error: (error) => console.error('Error loading gifs:', error),
-  //     });
-  // }
 
   getTrendingGifs(): Observable<IGif[]> {
     return this.http
