4cafcab9bbdf282e00912998c801ba008dafd8ef
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-18 09:34:40 -0600

    (Sección 7: Aplicación de Gifs) viewChild - Tomar referencias del template

--- 02-gifs-app/src/app/gifs/components/gifs-list/gifs-list.ts
+++ 02-gifs-app/src/app/gifs/components/gifs-list/gifs-list.ts
@@ -1,9 +1,8 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, ElementRef, input, viewChild } from '@angular/core';
 
 import { GifsListItem } from '@app/gifs/components/gifs-list-item/gifs-list-item';
 import { IGif } from '@app/gifs/interfaces/gif.interface';
-import { cn } from '@app/shared/utils/cn';
-import { Loading } from "@app/shared/components/loading/loading";
+import { Loading } from '@app/shared/components/loading/loading';
 
 type IViewState = 'loading' | 'empty' | 'data';
 
@@ -14,8 +13,8 @@ type IViewState = 'loading' | 'empty' | 'data';
 })
 export class GifsList {
   readonly gifs = input<IGif[] | null>();
-  readonly class = input<string>();
   readonly isLoading = input<boolean>(false);
+  readonly scrollDivRef = viewChild<ElementRef>('groupDiv');
 
   readonly viewState = computed<IViewState>(() => {
     if (this.isLoading()) return 'loading';
@@ -23,7 +22,8 @@ export class GifsList {
     return 'data';
   });
 
-  readonly rootClass = computed(() => {
-    return cn('columns-2 gap-4 space-y-4 md:columns-3 lg:columns-4 2xl:columns-5', this.class());
-  });
+  onScroll(e: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    console.log({scrollDiv});
+  }
 }
