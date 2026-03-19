428575c662cc0e0d06f898484fb0d84ca8c4e68b
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-03-18 21:23:07 -0600

    (Sección 9: Country SPA) DaisyUI - Componentes

--- 03-country-app/src/app/country/layouts/country-layout/country-layout.ts
+++ 03-country-app/src/app/country/layouts/country-layout/country-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "@app/shared/components/footer/footer";
 
 @Component({
   selector: 'app-country-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './country-layout.html',
 })
 export class CountryLayout {}
--- 03-country-app/src/app/shared/pages/home-page/home-page.ts
+++ 03-country-app/src/app/shared/pages/home-page/home-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { Footer } from "@app/shared/components/footer/footer";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
   styles: ``,
 })
