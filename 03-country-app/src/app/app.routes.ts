c96358450bf6a7e5fd54b2e5319dfa8d79f4e43d
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-18 18:07:50 -0600

    (Sección 9: Country SPA) Layout Components

--- 03-country-app/src/app/app.routes.ts
+++ 03-country-app/src/app/app.routes.ts
@@ -6,7 +6,6 @@ export const routes: Routes = [
     path: '',
     component: HomePage,
   },
-
   {
     path: 'country',
     loadChildren: () => import('./country/country.routes'),
--- 03-country-app/src/app/country/country.routes.ts
+++ 03-country-app/src/app/country/country.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
+import { CountryLayout } from './layouts/country-layout/country-layout';
 
 const countryRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('@app/country/pages/by-capital-page/by-capital-page'),
+    component: CountryLayout,
+    children: [
+      {
+        path: 'by-capital',
+        loadComponent: () => import('@app/country/pages/by-capital-page/by-capital-page'),
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ],
   },
   {
     path: '**',
