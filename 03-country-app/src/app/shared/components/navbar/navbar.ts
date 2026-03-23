21674a95a0eb96baad77a6249f4607a2507b3cd8
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-23 15:24:32 -0600

    feat: themes

--- 03-country-app/src/app/shared/components/navbar/navbar.ts
+++ 03-country-app/src/app/shared/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { changeTheme } from '@styleguide/core/utils';
 
 export type INavItem = {
   label: string;
@@ -22,5 +23,47 @@ export class Navbar {
       path: '/search',
       exact: false,
     },
+  ];
+
+  changeTheme() {
+    changeTheme('dracula');
+  }
+
+  readonly themes = [
+    { label: 'Light', value: 'light' },
+    { label: 'Night', value: 'night' },
+    { label: 'Dark', value: 'dark' },
+    { label: 'Cupcake', value: 'cupcake' },
+    { label: 'Bumblebee', value: 'bumblebee' },
+    { label: 'Emerald', value: 'emerald' },
+    { label: 'Corporate', value: 'corporate' },
+    { label: 'Synthwave', value: 'synthwave' },
+    { label: 'Retro', value: 'retro' },
+    { label: 'Cyberpunk', value: 'cyberpunk' },
+    { label: 'Valentine', value: 'valentine' },
+    { label: 'Halloween', value: 'halloween' },
+    { label: 'Garden', value: 'garden' },
+    { label: 'Forest', value: 'forest' },
+    { label: 'Aqua', value: 'aqua' },
+    { label: 'Lofi', value: 'lofi' },
+    { label: 'Pastel', value: 'pastel' },
+    { label: 'Fantasy', value: 'fantasy' },
+    { label: 'Wireframe', value: 'wireframe' },
+    { label: 'Black', value: 'black' },
+    { label: 'Luxury', value: 'luxury' },
+    { label: 'Dracula', value: 'dracula' },
+    { label: 'Cmyk', value: 'cmyk' },
+    { label: 'Autumn', value: 'autumn' },
+    { label: 'Business', value: 'business' },
+    { label: 'Acid', value: 'acid' },
+    { label: 'Lemonade', value: 'lemonade' },
+    { label: 'Coffee', value: 'coffee' },
+    { label: 'Winter', value: 'winter' },
+    { label: 'Dim', value: 'dim' },
+    { label: 'Nord', value: 'nord' },
+    { label: 'Sunset', value: 'sunset' },
+    { label: 'Caramellatte', value: 'caramellatte' },
+    { label: 'Abyss', value: 'abyss' },
+    { label: 'Silk', value: 'silk' },
   ];
 }
