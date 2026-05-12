5c7abe5925d53ec6d90cf6159ca1bb1de768393c
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-05-11 20:36:09 -0600

    (Sección 12: Pipes) - Uppercase, lowercase and titlecase pipes

--- 04-pipes-app/src/app/pages/basic-page/basic-page.ts
+++ 04-pipes-app/src/app/pages/basic-page/basic-page.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [RouterLink],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.html',
   styleUrl: './basic-page.css',
 })
-export default class BasicPage {}
+export default class BasicPage {
+  readonly nameLower = signal('PATRICIO');
+  readonly nameUpper = signal('PATRICIO');
+  readonly fullName = signal('PatricIo OsoRio');
+}
